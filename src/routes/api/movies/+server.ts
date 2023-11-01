7be75943a2fcda8b89ba0ce116e810faed2e2d5a
Nural, İsmail <mail@ismailnural.com>
2023-11-01 05:01:12 +0300

    Lint

--- src/routes/api/movies/+server.ts
+++ src/routes/api/movies/+server.ts
@@ -1,17 +1,19 @@
 import { json, error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
-import { API_URL, API_KEY } from '$env/static/private'
+import { API_URL, API_KEY } from '$env/static/private';
 
 export const GET: RequestHandler = async ({ fetch, url }) => {
   const page = url.searchParams.get('page') ?? '1';
   const query = url.searchParams.get('query') ?? '';
 
-  const response = await fetch(`${API_URL}/${query ? 'search/movie' : 'discover/movie'}?api_key=${API_KEY}&page=${page}&query=${query}`);
+  const response = await fetch(
+    `${API_URL}/${query ? 'search/movie' : 'discover/movie'}?api_key=${API_KEY}&page=${page}&query=${query}`,
+  );
 
   if (response.ok) {
     const resJSON = await response.json();
     return json(resJSON, {
-      status: 200
+      status: 200,
     });
   }
 
--- src/routes/movie/[id]/+page.server.ts
+++ src/routes/movie/[id]/+page.server.ts
@@ -1,13 +1,13 @@
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params }) => {
-    return {
-        movie: {
-            id: params.id,
-            title: 'Test Title',
-            description: 'Test Description'
-        },
-        title: 'Test Title',
-        description: 'Test Description'
-    };
+  return {
+    movie: {
+      id: params.id,
+      title: 'Test Title',
+      description: 'Test Description',
+    },
+    title: 'Test Title',
+    description: 'Test Description',
+  };
 };
