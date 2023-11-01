75c98cf3a522d13b90fcb7b8105baaa8006b4ec2
Nural, İsmail <mail@ismailnural.com>
2023-11-01 05:19:27 +0300

    Test

--- src/app.d.ts
+++ src/app.d.ts
@@ -6,7 +6,17 @@ declare global {
     // interface Locals {}
     // interface PageData {}
     // interface Platform {}
+
+    interface Platform {
+      env: {
+        COUNTER: DurableObjectNamespace;
+      };
+      context: {
+        waitUntil(promise: Promise<unknown>): void;
+      };
+      caches: CacheStorage & { default: Cache }
+    }
   }
 }
 
-export {};
+export { };
