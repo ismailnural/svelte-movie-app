0f0113d172e228b5d05348994d718bd890a2bf75
Nural, İsmail <mail@ismailnural.com>
2023-11-01 04:59:05 +0300

    Update api url env definition

--- src/routes/api/movies/+server.ts
+++ src/routes/api/movies/+server.ts
@@ -1,12 +1,12 @@
 import { json, error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
-import { BASE_URL, API_KEY } from '$env/static/private'
+import { API_URL, API_KEY } from '$env/static/private'
 
 export const GET: RequestHandler = async ({ fetch, url }) => {
   const page = url.searchParams.get('page') ?? '1';
   const query = url.searchParams.get('query') ?? '';
 
-  const response = await fetch(`${BASE_URL}/${query ? 'search/movie' : 'discover/movie'}?api_key=${API_KEY}&page=${page}&query=${query}`);
+  const response = await fetch(`${API_URL}/${query ? 'search/movie' : 'discover/movie'}?api_key=${API_KEY}&page=${page}&query=${query}`);
 
   if (response.ok) {
     const resJSON = await response.json();
